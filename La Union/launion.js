e83d3143779299aae3b5b237ee1505f8468a74db
Nicole_Torres2020 <danielacruztorres13@gmail.com>
2020-11-16 16:57:42 -0600

    DAW_Proyecto_de_Catedra

---
+++ La Union/launion.js
@@ -0,0 +1,43 @@
+//console.log('correcto');
+document.querySelector('#boton').addEventListener('click', traerdatos);
+
+function traerdatos(){
+    //console.log('dentro de la funcion');
+
+    const uno = new XMLHttpRequest();
+
+    uno.open('GET', 'launion.json', true);
+
+    uno.send();
+
+    uno.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            //console.log(this.responseText);
+            let datos = JSON.parse(this.responseText);
+            //console.log(datos);
+            let res = document.querySelector('#res');
+            res.innerHTML = '';
+
+            for(let item of datos){
+                //console.log(item);
+                res.innerHTML += `
+                <tr>
+                    <td>${item.Cabecera_Departamental}</td>
+                    <td>${item.Extencion_Territorial}</td>
+                    <td>${item.Fiestas_Patronales}</td>
+                    <td>${item.Numero_Habitantes}</td>
+                    <td>${item.Datos_Historicos}</td>
+                    <td>${item.Municipios}</td>
+                    <td>${item.Centros_Turisticos}</td>
+                    <td>${item.Lagos}</td>
+                    <td>${item.Rios}</td>
+                    <td>${item.Volcanes}</td>
+                    <td>${item.Cerros}</td>
+                    <td>${item.Personajes_Celebres}</td>
+                </tr>
+            
+          `
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DAW_Proyecto_de_Catedra
+ 
